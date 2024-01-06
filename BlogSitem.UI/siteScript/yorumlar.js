b3527d68cfbfc7c19edf062800a154f0e35da64b
Merve Bayindir <76398913+mervebayindir@users.noreply.github.com>
2024-01-06 23:55:40 +0300

    commit tekrar

--- BlogSitem.UI/siteScript/yorumlar.js
+++ BlogSitem.UI/siteScript/yorumlar.js
@@ -3,27 +3,23 @@ function YorumKaydet() {
 
     var makaleId = $("#makaleID").val();
     var yorum = $("#comment-message").val();
-    /*var kullanici = $("#kullaniciId").val();*/
-    debugger;
     $.ajax({
-        method: "post",
+        method: "POST",
         url: "/Makale/MakaleYorum",
-        data: { "makaleId": makaleId, "yorumIcerik": yorum }
-    }).done(function (data) {
-        if (data != null) {
-            debugger;
-            var yorumTarihi = new Date(data.yorumTarihi)
-            debugger;
-            var yorumBaslama = '<div class="comment"><div class="d-flex">';
-            var yorumResim = '<div class="comment-img"><img src="/template/assets/img/blog/comments-2.jpg"></div>';
-            var yorumAlani = '<div><h5><a href="">' + data.Adi + '</a> <a href="#" class="reply"><i class="bi bi-reply-fill"></i> Reply</a></h5><time datetime="' + yorumTarihi.toISOString() + '">' + yorumTarihi.toLocaleString() + '</time><p>' + data.Yorum + '</p></div></div></div>';
+        data: { "makaleId": makaleId, "yorumIcerik": yorum },
+        success: function (data) {
+            if (data != null) {
+                var yorumTarihi = new Date(data.yorumTarihi);
 
-            $(".comment:last").append(yorumBaslama, yorumResim, yorumAlani);
-            location.reload();
+                var yorumBaslama = '<div class="comment"><div class="d-flex">';
+                var yorumResim = '<div class="comment-img"><img src="/template/assets/img/blog/comments-2.jpg"></div>';
+                var yorumAlani = '<div><h5><a href="">' + data.adi + '</a> <a href="#" class="reply"><i class="bi bi-reply-fill"></i> Yanıtla</a></h5><time datetime="' + yorumTarihi.toISOString() + '">' + yorumTarihi.toLocaleString() + '</time><p>' + data.yorum + '</p></div></div></div>';
+
+                $(".comment:last").append(yorumBaslama, yorumResim, yorumAlani);
+            }
+        },
+        error: function () {
+           
         }
-
-    }).fail(function () {
-
-
     });
 };
